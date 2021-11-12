2e61adb9e1af5055dbb510f9b277003031214298
CLR60 <79085963+CLR60@users.noreply.github.com>
2021-11-12 11:56:58 +0100

    first commit

---
+++ GestionUtilisateurs/UtilisateursBO/Utilisateur.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UtilisateursBO
+{
+    public class Utilisateur
+    {
+        private int Id_utilisateur;
+        private string Nom_utilisateur;
+
+        public Utilisateur(string Nom_utilisateur)
+        {
+            Nom_utilisateur = this.Nom_utilisateur;
+        }
+        
+        public Utilisateur(int Id_utilisateur, string Nom_utilisateur)
+        {
+            Id_utilisateur = this.Id_utilisateur;
+            Nom_utilisateur = this.Nom_utilisateur;
+        }
+
+        public int Id   
+        {
+            get => this.Id_utilisateur ;
+            set => this.Id_utilisateur = Id_utilisateur;
+        }
+
+        public string Nom    
+        {
+            get => this.Nom_utilisateur;
+            set => this.Nom_utilisateur = Nom_utilisateur;
+        }
+
+    }
+}
