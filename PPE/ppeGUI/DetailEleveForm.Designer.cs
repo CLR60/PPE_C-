1bd398a94157c784b0a37a21e5781cd25ed93653
CLR60 <79085963+CLR60@users.noreply.github.com>
2021-11-23 10:00:08 +0100

    finalisation Form

--- PPE/ppeGUI/DetailEleveForm.Designer.cs
+++ PPE/ppeGUI/DetailEleveForm.Designer.cs
@@ -28,19 +28,175 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.NomDetailText = new System.Windows.Forms.TextBox();
+            this.PrenomDetailText = new System.Windows.Forms.TextBox();
+            this.ClasseDetailText = new System.Windows.Forms.TextBox();
+            this.NaissanceDetailText = new System.Windows.Forms.TextBox();
+            this.SanteDetailText = new System.Windows.Forms.TextBox();
+            this.TelPDetailText = new System.Windows.Forms.TextBox();
+            this.TelEDetailText = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(47, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nom :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 98);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Prénom :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(34, 137);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Classe :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(0, 191);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(134, 17);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Date de naissance :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(34, 240);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(100, 17);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Etat de santé :";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(52, 287);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(82, 17);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Tel Parent :";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(53, 344);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(81, 17);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Tel Enfant :";
+            // 
+            // NomDetailText
+            // 
+            this.NomDetailText.Location = new System.Drawing.Point(157, 41);
+            this.NomDetailText.Name = "NomDetailText";
+            this.NomDetailText.Size = new System.Drawing.Size(100, 22);
+            this.NomDetailText.TabIndex = 7;
+            // 
+            // PrenomDetailText
+            // 
+            this.PrenomDetailText.Location = new System.Drawing.Point(157, 93);
+            this.PrenomDetailText.Name = "PrenomDetailText";
+            this.PrenomDetailText.Size = new System.Drawing.Size(100, 22);
+            this.PrenomDetailText.TabIndex = 8;
+            // 
+            // ClasseDetailText
+            // 
+            this.ClasseDetailText.Location = new System.Drawing.Point(157, 137);
+            this.ClasseDetailText.Name = "ClasseDetailText";
+            this.ClasseDetailText.Size = new System.Drawing.Size(100, 22);
+            this.ClasseDetailText.TabIndex = 9;
+            // 
+            // NaissanceDetailText
+            // 
+            this.NaissanceDetailText.Location = new System.Drawing.Point(157, 191);
+            this.NaissanceDetailText.Name = "NaissanceDetailText";
+            this.NaissanceDetailText.Size = new System.Drawing.Size(100, 22);
+            this.NaissanceDetailText.TabIndex = 10;
+            // 
+            // SanteDetailText
+            // 
+            this.SanteDetailText.Location = new System.Drawing.Point(157, 240);
+            this.SanteDetailText.Name = "SanteDetailText";
+            this.SanteDetailText.Size = new System.Drawing.Size(100, 22);
+            this.SanteDetailText.TabIndex = 11;
+            // 
+            // TelPDetailText
+            // 
+            this.TelPDetailText.Location = new System.Drawing.Point(157, 287);
+            this.TelPDetailText.Name = "TelPDetailText";
+            this.TelPDetailText.Size = new System.Drawing.Size(100, 22);
+            this.TelPDetailText.TabIndex = 12;
+            // 
+            // TelEDetailText
+            // 
+            this.TelEDetailText.Location = new System.Drawing.Point(157, 344);
+            this.TelEDetailText.Name = "TelEDetailText";
+            this.TelEDetailText.Size = new System.Drawing.Size(100, 22);
+            this.TelEDetailText.TabIndex = 13;
             // 
             // DetailEleveForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.TelEDetailText);
+            this.Controls.Add(this.TelPDetailText);
+            this.Controls.Add(this.SanteDetailText);
+            this.Controls.Add(this.NaissanceDetailText);
+            this.Controls.Add(this.ClasseDetailText);
+            this.Controls.Add(this.PrenomDetailText);
+            this.Controls.Add(this.NomDetailText);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Name = "DetailEleveForm";
             this.Text = "Detail Eleve";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox NomDetailText;
+        private System.Windows.Forms.TextBox PrenomDetailText;
+        private System.Windows.Forms.TextBox ClasseDetailText;
+        private System.Windows.Forms.TextBox NaissanceDetailText;
+        private System.Windows.Forms.TextBox SanteDetailText;
+        private System.Windows.Forms.TextBox TelPDetailText;
+        private System.Windows.Forms.TextBox TelEDetailText;
     }
 }
