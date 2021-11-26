6a5089644ad4afbf209fa553bcfca78108234dc9
N0SAFE <77668394+N0SAFE@users.noreply.github.com>
2021-11-26 08:26:37 +0100

    commit implement bdd

--- PPE/ppeBO/UtilisateurBO.cs
+++ PPE/ppeBO/UtilisateurBO.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 
 namespace ppeBO
 {
-    public class Class1
+    public class Utilisateur
     {
+        
     }
 }
--- PPE/ppeDAl/UtilisateurDAO.cs
+++ PPE/ppeDAl/UtilisateurDAO.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 
 namespace ppeDAL
 {
-    class Class2
+    class UtilisateurDAO
     {
+        
     }
 }
